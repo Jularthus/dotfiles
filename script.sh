ef7ab61deb4aa3eb11fc30b8a759378aaffb5fba
Jularthus <59778433+Jularthus@users.noreply.github.com>
2024-12-11 18:25:08 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,2 @@
+sed -i -e 's/ line, column);/ line, (unsigned int)column);/g' ~/epita-prepa-computer-science-proj-s3-2028-stg-Canard-Brillant-Ampute/src/Finders/blob_process.c
+  sed -i -e 's/ line, column);/ line, (unsigned int)column);/g' ~/epita-prepa-computer-science-proj-s3-2028-stg-Canard-Brillant-Ampute/src/Finders/mainGroupiert.c
