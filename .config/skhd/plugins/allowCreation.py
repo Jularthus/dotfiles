bdb6ccaf3675a702b3f076f92627564e3d3356d4
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2025-04-28 23:08:15 +0200

    updated skhd scripts ; renamed i3 to epita

--- .config/skhd/plugins/allowCreation.py
+++ .config/skhd/plugins/allowCreation.py
@@ -1,9 +1,9 @@
 import subprocess
+from os import system
 
 spacesCmd = subprocess.run(["yabai", "-m", "query", "--spaces"], stdout=subprocess.PIPE)
 spacesNumber = len(spacesCmd.stdout.decode("utf-8").split("{")) - 1
 
-from os import system
 
 if spacesNumber < 10:
     system("yabai -m space --create")
--- .config/skhd/plugins/killEmptySpaces.py
+++ .config/skhd/plugins/killEmptySpaces.py
@@ -1,13 +1,26 @@
+# USE THIS VERSION
 import subprocess
 from os import system
 
-print('')
-
 spacesCmd = subprocess.run(["yabai", "-m", "query", "--spaces"], stdout=subprocess.PIPE)
-splitedOut = spacesCmd.stdout.decode('utf-8').split('windows":[')
+splitedOut = spacesCmd.stdout.decode("utf-8").split('"first-window":')
 index = -1
 for i in splitedOut:
-    index+=1
-    if(i[0] == ']'):
-        system(f'yabai -m space {index} --destroy')
-        index-=1
+    index += 1
+    if i[0] == "0":
+        system(f"yabai -m space {index} --destroy")
+        index -= 1
+
+
+# DEPRECATED OLD VESRION
+# import subprocess
+# from os import system
+
+# spacesCmd = subprocess.run(["yabai", "-m", "query", "--spaces"], stdout=subprocess.PIPE)
+# splitedOut = spacesCmd.stdout.decode("utf-8").split('windows":[')
+# index = -1
+# for i in splitedOut:
+#     index += 1
+#     if i[0] == "]":
+#         system(f"yabai -m space {index} --destroy")
+#         index -= 1
