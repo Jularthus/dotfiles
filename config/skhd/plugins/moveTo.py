72e52f8ce87315a9ee08c5d59ddef5f327ae05a0
Jularthus <59778433+Jularthus@users.noreply.github.com>
2025-07-01 17:16:36 +0200

    Add files via upload

--- config/skhd/plugins/moveTo.py
+++ config/skhd/plugins/moveTo.py
@@ -7,12 +7,17 @@ arg = int(argv[1])
 spacesCmd = subprocess.run(["yabai", "-m", "query", "--spaces"], stdout=subprocess.PIPE)
 spacesNumber = len(spacesCmd.stdout.decode("utf-8").split("{")) - 1
 
-if arg <= spacesNumber:
-    if system(f"yabai -m space --focus {arg}"):
-        system(
-            f"yabai -m query --spaces --space {arg}  | jq -r '.windows[0]' | xargs yabai -m window --focus"
-        )
-else:
-    for _ in range(arg - spacesNumber):
-        system("yabai -m space --create")
-    system(f"yabai -m space --focus {arg}")
+for _ in range(arg - spacesNumber):
+    system("yabai -m space --create")
+
+queryFullscreen = subprocess.run(
+    f"yabai -m query --spaces --space {arg}".split(" "), stdout=subprocess.PIPE
+)
+isFullscreen = bool(
+    '"is-native-fullscreen":true' in queryFullscreen.stdout.decode("utf-8")
+)
+
+if system(f"yabai -m space --focus {arg - 1 if isFullscreen else arg}"):
+    system(
+        f"yabai -m query --spaces --space {arg}  | jq -r '.windows[0]' | xargs yabai -m window --focus"
+    )
